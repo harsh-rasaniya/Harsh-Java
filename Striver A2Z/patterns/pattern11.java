2eac8866b19d849d916b321d34ec4577998f7904
harsh-rasaniya <harshrasaniya393@gmail.com>
2026-08-25 09:21:22 +0530

    Add pattern 11 solution

---
+++ Striver A2Z/patterns/pattern11.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class pattern11{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int n = sc.nextInt();
+        for(int i=0 ; i<n ; i++){
+            for(int j=0;j<=i;j++){
+                if((i+j)%2==0) System.out.print(1);
+                else System.out.print(0);
+            }
+            System.out.println();
+        }
+    }
+}
