934324c987a000dc20f6d9857425ad439e20b38b
harsh-rasaniya <harshrasaniya393@gmail.com>
2026-08-25 09:20:32 +0530

    Add pattern 9 solution

---
+++ Striver A2Z/patterns/pattern9.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class pattern9{
+    public static void pyramid(int n){
+        for(int i=0 ; i<n ; i++){
+            for(int j=0 ; j<n-i-1 ; j++) System.out.print(" ");
+            for(int j=0 ; j<(2*i)+1 ; j++) System.out.print("*");
+            for(int j=0 ; j<i ; j++)System.out.print(" ");
+            System.out.println();
+        }
+    }
+    public static void invertedPyramid(int n ){
+        for(int i = n-1 ; i >= 0 ; i-- ){
+            for(int j=0 ; j< n-i-1 ; j++) System.out.print(" ");
+            for(int j=0 ; j<(2*i)+1 ; j++) System.out.print("*");
+            for(int j=0 ; j< n-i ; j++) System.out.print(" ");
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter  a  number  :  ");
+        int n = sc.nextInt();
+        pyramid(n);
+        invertedPyramid(n);
+    }
+}
